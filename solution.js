fa17933572b9127194608fe2015327db6ea5c832
YouKillingMe <mrmisfortune14@gmail.com>
2015-06-10 18:59:23 +0300

    added script, button onclick event

--- solution.js
+++ solution.js
@@ -1,14 +1,17 @@
-var D = b^2 - 4*a*c;
-if (D = 0) {
-	x1= -b/(2*a);
-	x2= x1;
+var solution = function() {
+var a=document.getElementById("valueA").value, b=document.getElementById("valueB").value, c=document.getElementById("valueC").value;
+var D = b*b - 4*a*c; 
+if (D == 0) {
+	document.getElementById("x1").innerHTML= "x1=" + (-b/(2*a));
+	document.getElementById("x2").innerHTML= "x2=" + (-b/(2*a));
 }
 else {
 	if (D > 0) {
-		x1 = (-b + Math.sqrt(D)) / (2*a);
-		x2 = (-b - Math.sqrt(D)) / (2*a);
+		document.getElementById("x1").innerHTML= "x1=" + ((-b + Math.sqrt(D)) / (2*a));
+		document.getElementById("x2").innerHTML= "x2=" + ((-b - Math.sqrt(D)) / (2*a));
 	}
 	else {
 		alert("Корней на множестве действительных чисел нет!")
 	}
+}
 }
